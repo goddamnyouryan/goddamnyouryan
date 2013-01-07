b5a851c9e7bf4d425f61e048fcfdac00ff936bb6
Ryan MacInnes <ryan.macinnes@gmail.com>
2013-01-07 13:58:32 -0500

    Fixing issue with www for seo.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Goddamnyouryan
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Dealing with non www requests
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.middleware.use "NoWww"
   end
 end
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.goddamnyouryan.com"
+SitemapGenerator::Sitemap.default_host = "http://goddamnyouryan.com"
 
 SitemapGenerator::Sitemap.create do
   add '/blog'
