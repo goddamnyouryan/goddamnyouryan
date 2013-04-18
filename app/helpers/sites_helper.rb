298beabf74be9f2d67ed518fb863f2ea18ab754a
Ryan MacInnes <ryan.macinnes@gmail.com>
2013-04-18 00:59:44 -0400

    Making the site more awesome.

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,6 @@
 module SitesHelper
+
+  def random_submit_label
+    ['Shoot', 'Fuck It', 'Kablamo', 'Awwww yeahhh', 'Whatever', 'Yes, Please', 'Might as well, eh?', 'Sexyness', 'Do It Already'].shuffle.first
+  end
 end
