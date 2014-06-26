32accdac44c8be1f9495bf703c43cd2451d4850c
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-06-26 12:36:27 -0700

    Dynamic title on blog posts.

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,2 +1,14 @@
 module BlogHelper
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
+
+  def blog_title
+    headline = 'Goddamn You Ryan MacInnes - A Kickass Blog for a Kickass Dude'
+    if content_for?(:title)
+      [content_for(:title), ' - ', headline].join
+    else
+      headline
+    end
+  end
 end
