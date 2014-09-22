7d029634e7c43add245d9938d33282cd038b77d6
Ryan MacInnes <ryan.macinnes@gmail.com>
2014-09-21 23:43:25 -0700

    Adding paperclip validator.

--- app/models/site.rb
+++ app/models/site.rb
@@ -6,6 +6,7 @@ class Site < ActiveRecord::Base
                             storage: :s3, s3_credentials: "#{Rails.root}/config/s3.yml",
                             path: ':id/:style', s3_host_alias: 'assets.goddamnyouryan.com',
                             url: ':s3_alias_url'
+  validates_attachment_content_type :photo, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
   scope :ordered, -> { order('position') }
   scope :active, -> { where(status: 'active').ordered }
